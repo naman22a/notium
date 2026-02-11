4f5003ad06423bb8c45e5c4e381fc8c52318f9ce
Naman Arora <namanarora1022@gmail.com>
2026-02-11 21:35:25 +0530

    feat: add back button

--- lib/screens/note_editing_screen.dart
+++ lib/screens/note_editing_screen.dart
@@ -14,6 +14,7 @@ class _NoteEditingScreenState extends State<NoteEditingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Column(
         children: [
           QuillSimpleToolbar(
