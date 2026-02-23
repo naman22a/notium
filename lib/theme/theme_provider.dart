3bb99c4533a5e812a657bd3e2a8bd690ee017a74
Naman Arora <namanarora1022@gmail.com>
2026-02-23 13:41:46 +0530

    feat: persist dark mode in hive

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:notium/theme/theme.dart';
 
 class ThemeProvider with ChangeNotifier {
@@ -8,19 +9,38 @@ class ThemeProvider with ChangeNotifier {
   ThemeData get themeData => _themeData;
   bool get isDarkMode => _isDarkMode;
 
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  /// Load saved theme from Hive
+  void _loadTheme() {
+    final box = Hive.box('settings');
+    _isDarkMode = box.get('darkMode', defaultValue: false);
+    _themeData = _isDarkMode ? darkMode : lightMode;
+  }
+
+  /// Setter if manually setting theme
   set themeData(ThemeData themeData) {
     _themeData = themeData;
     notifyListeners();
   }
 
+  /// Setter if manually setting dark mode
   set isDarkMode(bool value) {
     _isDarkMode = value;
+    _themeData = value ? darkMode : lightMode;
+    Hive.box('settings').put('darkMode', value);
     notifyListeners();
   }
 
+  /// Toggle theme + persist
   void toggleTheme() {
     _isDarkMode = !_isDarkMode;
     _themeData = _isDarkMode ? darkMode : lightMode;
+
+    Hive.box('settings').put('darkMode', _isDarkMode);
+
     notifyListeners();
   }
 }
