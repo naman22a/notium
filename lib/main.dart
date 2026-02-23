48b6d5cab9ea4163dbaaced9ed8151eb8b46fd17
Naman Arora <namanarora1022@gmail.com>
2026-02-23 11:48:51 +0530

    feat: settings font size change implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notium/adapters/app_font_size_adapter.dart';
 import 'package:notium/models/note_model.dart';
+import 'package:notium/providers/font_provider.dart';
 import 'package:notium/providers/picked_color_provider.dart';
 import 'package:notium/screens/home_screen.dart';
 import 'package:provider/provider.dart';
@@ -13,8 +15,10 @@ Future<void> main() async {
   Hive.registerAdapter(NoteModelAdapter());
   Hive.registerAdapter(TrashNoteModelAdapter());
   Hive.registerAdapter(ColorAdapter());
+  Hive.registerAdapter(AppFontSizeAdapter());
   await Hive.openBox<NoteModel>('notes');
   await Hive.openBox<TrashNoteModel>('trash');
+  await Hive.openBox('settings');
 
   runApp(
     MultiProvider(
@@ -24,6 +28,9 @@ Future<void> main() async {
         ),
         ChangeNotifierProvider(
           create: (context) => PickedColorProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => FontProvider(),
         ),
       ],
       child: MyApp(),
@@ -36,10 +43,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: Provider.of<ThemeProvider>(context).themeData,
-      home: HomeScreen(),
+    return Consumer<FontProvider>(
+      builder: (context, fontProvider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: Provider.of<ThemeProvider>(context).themeData,
+          builder: (context, child) {
+            return MediaQuery(
+              data: MediaQuery.of(context)
+                  .copyWith(textScaleFactor: fontProvider.scale),
+              child: child!,
+            );
+          },
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
